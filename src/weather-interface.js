00b984f57554f604e4e908d76222d767906648d8
Benjamin Thom <jaminthom@gmail.com>
2020-02-11 22:52:31 -0800

    comment all ui and business logic within weather-interface.js

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
     const city = $('#location').val();
     $('#location').val("");
 
-    asyncApiCall();
-
     (async () => {
-      let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`);
+      // The API call is business logic.
+      let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
       let jsonifiedResponse = await response.json();
+      // The getElements method is UI logic so it will need to be separated from the business logic.  
       getElements(jsonifiedResponse);
     })();
 
