73c32733df9bd6941a0b7ccc10a8351d2a21343c
Benjamin Thom and Kristina Hengster Tintor <benjamin+kristina@>
2020-02-12 11:23:34 -0800

    rename weather-interface.js to main.js and include imports for jquery, bootstrap, bootstrap.min.css, and styles.css

--- src/main.js
+++ src/main.js
@@ -2,3 +2,40 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { WeatherService } from './../src/weather-service.js';
+
+$(document).ready(function() {
+
+  $('#weatherLocation').click(function() {
+    const city = $('#location').val();
+    $('#location').val("");
+
+    (async () => {
+      let weatherService = new WeatherService();
+      const response = await weatherService.getWeatherByCity(city);
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
+
+  });
+});
+
+
+//Tips from Dusty:
+
+//Tip #1
+// fetch('api string w/ API_KEY').then((responce) => {responce.json()}).then((obj) => {
+//   let questions = obj.results[0].question;
+// });
+
+//Tip #2
+// async function bears() {
+//    let apiCall = await fetch('api string w/ API_KEY');
+//   let obj = await apiCall.json();
+
+//   return obj;
+// }
