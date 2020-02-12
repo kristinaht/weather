fe1820ddc77ff47ad45fc8c93696ee07c2fc1d94
Benjamin Thom <jaminthom@gmail.com>
2020-02-11 23:04:30 -0800

    create weather-service.js to store all oof our business ineterfaces logic. update weather-interface to only include out UI logic, and weather-service.js to only include our business logic

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -1,20 +1,22 @@
+import { WeatherService } from './../src/weather-service.js';
+
 $(document).ready(function() {
+
   $('#weatherLocation').click(function() {
     const city = $('#location').val();
     $('#location').val("");
 
     (async () => {
-      // The API call is business logic.
-      let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`);
-      let jsonifiedResponse = await response.json();
-      // The getElements method is UI logic so it will need to be separated from the business logic.  
-      getElements(jsonifiedResponse);
+      let weatherService = new WeatherService();
+      const response = await weatherService.getWeatherByCity(city);
+      getElements(response);
     })();
 
-    const getElements = function(response) {
+    function getElements(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
     }
+
   });
 });
 
